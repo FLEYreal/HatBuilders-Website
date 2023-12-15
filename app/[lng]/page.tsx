a5b2cd0a5a9aa5f0a265bdf07c58ba0f03fb19a0
FLEYreal <borisov.nikita.off@gmail.com>
2023-12-15 18:39:31 +0800

    Theme corrections

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -6,7 +6,7 @@ import { useTranslation } from '@/shared/i18n'
 //import { useTranslation } from '@/shared/i18n/client';
 
 // Material-UI
-import { Typography } from '@mui/material'
+import { Button, Typography } from '@mui/material'
 
 // Widgets
 import { ThemeSelector } from '@/widgets/theme-selector'
@@ -25,7 +25,11 @@ export default async function Home({ params: { lng } }: { params: { lng: string 
         <Typography component='main'>
             <LanguageSelector lng={lng} type='button' menu={undefined} />
             <ThemeSelector />
-            Test: {t('our_portfolio')}
+            <Button color='primary' variant='contained'>PRIMARY</Button>
+            <Button color='secondary' variant='contained'>SECONDARY</Button>
+            <Button color='info' variant='contained'>INFO</Button>
+            <Button color='warning' variant='contained'>WARNING</Button>
+            <Button color='error' variant='contained'>ERROR</Button>
         </Typography>
     )
 }
--- shared/mui/theme.ts
+++ shared/mui/theme.ts
@@ -1,85 +1,55 @@
 // Material-UI
-import { createTheme } from '@mui/material/styles';
+import { createTheme, Theme } from '@mui/material/styles';
 
-// Utils
-import Coloring from './Coloring';
+export const palette = {
+    primary: {
+        main: '#26F153',
+        light: '#6FFF8E',
+        dark: '#0DBA33',
+    },
+    secondary: {
+        main: '#EC264A',
+        light: '#F84666',
+        dark: '#B40F2D'
+    },
+    info: {
+        main: '#03A9F4',
+        light: '#1CB8FF',
+        dark: '#0288D1'
 
-// Main variables for colors
-export const $primary = '#5FA60C';
-export const $secondary = '#2D8721';
-export const $info = '#01828C';
-export const $warning = '#FFAB28';
-export const $error = '#FF0300';
+    },
+    warning: {
+        main: '#ECB526',
+        light: '#FFC93F',
+        dark: '#CD9F28'
+    },
+    error: {
+        main: '#D32F2F',
+        light: '#EF5350',
+        dark: '#B91818'
+    },
+}
+
+export const transitions = {
+    duration: {
+        shortest: 75,
+        shorter: 100,
+        short: 150,
+        standard: 100,
+        complex: 275,
+        enteringScreen: 225,
+        leavingScreen: 195,
+    }
+}
 
 // Custom dark theme
 export const darkTheme = createTheme({
     palette: {
         mode: 'dark',
-        primary: {
-            main: Coloring.lightenColor($primary, 0.25),
-            light: Coloring.darkenColor($primary, 0.55),
-            dark: Coloring.darkenColor($primary, 0.025),
-        },
-        secondary: {
-            main: Coloring.lightenColor($secondary, 0.25),
-            light: Coloring.lightenColor($secondary, 0.55),
-            dark: Coloring.darkenColor($secondary, 0.025)
-        },
-        info: {
-            main: Coloring.lightenColor($info, 0.25),
-            light: Coloring.lightenColor($info, 0.55),
-            dark: Coloring.darkenColor($info, 0.025)
-
-        },
-        warning: {
-            main: Coloring.lightenColor($warning, 0.25),
-            light: Coloring.lightenColor($warning, 0.55),
-            dark: Coloring.darkenColor($warning, 0.025)
-        },
-        error: {
-            main: Coloring.lightenColor($error, 0.25),
-            light: Coloring.lightenColor($error, 0.55),
-            dark: Coloring.darkenColor($error, 0.025)
-        },
+        ...palette
     },
-    shadows: [
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-    ],
-    transitions: {
-        duration: {
-            shortest: 75,
-            shorter: 100,
-            short: 150,
-            standard: 100,
-            complex: 275,
-            enteringScreen: 225,
-            leavingScreen: 195,
-        }
-    },
+    shadows: Array(25).fill('none') as Theme['shadows'],
+    transitions: transitions,
     shape: {
         borderRadius: 8
     }
@@ -89,71 +59,10 @@ export const darkTheme = createTheme({
 export const lightTheme = createTheme({
     palette: {
         mode: 'light',
-        primary: {
-            main: Coloring.lightenColor($primary, 0.25),
-            light: Coloring.darkenColor($primary, 0.55),
-            dark: Coloring.darkenColor($primary, 0.025),
-        },
-        secondary: {
-            main: Coloring.lightenColor($secondary, 0.25),
-            light: Coloring.lightenColor($secondary, 0.55),
-            dark: Coloring.darkenColor($secondary, 0.025)
-        },
-        info: {
-            main: Coloring.lightenColor($info, 0.25),
-            light: Coloring.lightenColor($info, 0.55),
-            dark: Coloring.darkenColor($info, 0.025)
-
-        },
-        warning: {
-            main: Coloring.lightenColor($warning, 0.25),
-            light: Coloring.lightenColor($warning, 0.55),
-            dark: Coloring.darkenColor($warning, 0.025)
-        },
-        error: {
-            main: Coloring.lightenColor($error, 0.25),
-            light: Coloring.lightenColor($error, 0.55),
-            dark: Coloring.darkenColor($error, 0.025)
-        },
+        ...palette
     },
-    shadows: [
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-        'none',
-    ],
-    transitions: {
-        duration: {
-            shortest: 75,
-            shorter: 100,
-            short: 150,
-            standard: 100,
-            complex: 275,
-            enteringScreen: 225,
-            leavingScreen: 195,
-        }
-    },
+    shadows: Array(25).fill('none') as Theme['shadows'],
+    transitions: transitions,
     shape: {
         borderRadius: 8
     }
