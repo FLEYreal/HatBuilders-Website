b8fb72c3b580c2dc4843f4185b6db3341033fec8
FLEYreal <borisov.nikita.off@gmail.com>
2024-01-05 18:34:52 +0800

    Article decomposition

---
+++ features/article/ui/block/image.tsx
@@ -0,0 +1,22 @@
+// Basics
+import React from 'react';
+import Image from 'next/image'
+
+// Config
+import { ImageInterface } from '../../config/types';
+
+/**
+ * ArticleBlock: Block for images. Enables you to properly insert image into an article so it fit well!
+ */
+export const Picture = ({
+    src,
+    alt = "Image from Article"
+}: ImageInterface) => {
+    return (
+        <Image src={src} alt={alt} style={{
+            width: '100%',
+            margin: "10px 0",
+            borderRadius: '0'
+        }} />
+    )
+}
