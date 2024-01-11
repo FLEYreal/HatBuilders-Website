e4b0d2a4f1a8b093ee9cf94c358fd57a8ebc9186
FLEYreal <borisov.nikita.off@gmail.com>
2024-01-11 21:22:30 +0800

    Adaptivity to about page is completed

--- paging/main/modules/about/styled-comps.tsx
+++ paging/main/modules/about/styled-comps.tsx
@@ -43,21 +43,34 @@ const StyledWrapperComponent = styled(Flex) <FlexInterface>`
 // ILLUSTRATION STYLING
 const StyledIllustration = styled(({ def, ...props }: IllustrationInterface) => e(Box, props)) <IllustrationInterface>`
     position: absolute;
-    right: 0;
     background-image: url(${dio.src});
     background-size: cover;
     z-index: -1;
 
+    ${({ def }) => def!.b('xs')} {
+        top: 6vh;
+        right: calc(50vw - 125px);
+        width: 250px;
+        height: 160px;
+    }
 
-    
+    ${({ def }) => def!.b('md')} {
+        top: 4vh;
+        right: calc(50vw - 210px);
+        width: 420px;
+        height: 280px;
+    }
+
     ${({ def }) => def!.b('lg')} {
         top: calc(50vh - 220px);
+        right: 0;
         width: 440px;
         height: 440px;
     }
 
     ${({ def }) => def!.b('xl')} {
         top: calc(50vh - 340px);
+        right: 0;
         width: 680px;
         height: 680px;
     }
@@ -78,8 +91,17 @@ const StyledPrimaryDivider = styled(({ def, ...props }: PrimaryDividerInterface)
 const StyledArticleComponent = styled(({ def, ...props }: StyledArticleInterface) => e(Article, props)) <StyledArticleInterface>`
     background: ${({ def }) => def!.t.palette.background.default};
 
-    ${({ def }) => def!.b('lg')} { box-shadow: 0px 0px 100px 150px ${({ def }) => def!.t.palette.background.default}; }
-    ${({ def }) => def!.b('xl')} { box-shadow: 0px 0px 300px 400px ${({ def }) => def!.t.palette.background.default}; }
+    ${({ def }) => def!.b('xs')} { 
+        box-shadow: 0px 0px 50px 50px ${({ def }) => def!.t.palette.background.default};
+        margin-top: 14vh;
+    }
+    ${({ def }) => def!.b('lg')} { 
+        box-shadow: 0px 0px 100px 150px ${({ def }) => def!.t.palette.background.default};
+        margin-top: 0;
+    }
+    ${({ def }) => def!.b('xl')} {
+        box-shadow: 0px 0px 300px 400px ${({ def }) => def!.t.palette.background.default};
+    }
 `
 
 const ArticleComponent = ({ children, ...props }: StyledArticleInterface) => {
